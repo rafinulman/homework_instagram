3cc975b120a3c185486f1f6dff139a2776a5c027
rafinulman <rafinulman@gmail.com>
2014-11-08 19:18:37 -0600

    Added CRUD functionality

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -3,5 +3,59 @@ class PhotosController < ApplicationController
     @list_of_photos = Photo.all
   end
 
+  def show
+  	@photo_id = params[:id]
+  	@photo_source = Photo.find(params[:id]).source
+  	@photo_caption = Photo.find(params[:id]).caption
+  end
+
+  def new_form
+  end
+
+  def new_row
+  	@photo = Photo.new
+  	@photo.caption = params[:the_caption]
+  	@photo.source = params[:the_source]
+  	
+  	@photo.save
+
+  	@photo_id = @photo.id
+  	@photo_caption = params[:the_caption]
+  	@photo_source = params[:the_source]
+
+  	render "show"
+  end
+
+  def destroy
+  	@photo = Photo.find(params[:id])
+  	@photo.destroy
+
+  	@list_of_photos = Photo.all
+
+  	render "index"
+
+  end
+
+  def edit_form
+  	@photo_id = params[:id]
+  	@photo_source = Photo.find(params[:id]).source
+  	@photo_caption = Photo.find(params[:id]).caption
+  end
+
+  def update
+  	@photo = Photo.find(params[:id])
+
+  	@photo.caption = params[:the_caption]
+  	@photo.source = params[:the_source]
+  	
+  	@photo.save
+
+  	@photo_id = @photo.id
+  	@photo_caption = params[:the_caption]
+  	@photo_source = params[:the_source]
+
+  	render "show"
+
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,20 @@
 Rails.application.routes.draw do
+	# Landing page
+		get("/", { :controller => "photos", :action => "index" })
 
-  get("/", { :controller => "photos", :action => "index" })
+	# Routes to READ photos
+		get("/all_photos", { :controller => "photos", :action => "index" })
+		get("/photo_details/:id", { :controller => "photos", :action => "show" })
 
-  # Routes to READ photos
-  get("/all_photos", { :controller => "photos", :action => "index" })
-  get("/photo_details/:id", { :controller => "photos", :action => "show" })
+	# Routes to CREATE photos
+		get("/new_photo_form", { :controller => "photos", :action => "new_form" })
+		get("/New_photo", { :controller => "photos", :action => "new_row"})
+
+	# Routes to DESTROY photos
+		get("/delete_photo/:id", {:controller => "photos", :action => "destroy"})
+
+	# Routes to EDIT photos
+		get("/edit_photo/:id", {:controller => "photos", :action => "edit_form"})
+		get("/update_photo/:id", {:controller => "photos", :action => "update"})
 
 end
